ce5ef8b4a2cc38ae02f366e35bdd78b39638f494
mlamraniabouelassad4336-dotcom <m.lamraniabouelassad4336@uca.ac.ma>
2026-04-21 21:56:57 +0100

    feat: add subtract function

--- src/math.js
+++ src/math.js
@@ -8,5 +8,8 @@ function factorial(n) {
 if (n < 0) throw new Error('Nombre négatif');
 if (n <= 1) return 1;
 return n * factorial(n - 1);
+function subtract(a, b) {
+  return a - b;
+}
 }
 module.exports = { add, multiply, factorial };
